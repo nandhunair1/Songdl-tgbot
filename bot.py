3d9f663c0472a0ca5a8898a3f8adcf6bf3327208
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-17 15:29:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+#    Copyright (c) 2021 Infinity BOTs <https://t.me/Infinity_BOTs>
+ 
+#    This program is free software: you can redistribute it and/or modify  
+#    it under the terms of the GNU General Public License as published by  
+#    the Free Software Foundation, version 3.
+# 
+#    This program is distributed in the hope that it will be useful, but 
+#    WITHOUT ANY WARRANTY; without even the implied warranty of 
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#    General Public License for more details.
+
 import os
 import aiohttp
 from pyrogram import filters, Client
