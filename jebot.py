b9f52a84cc0963cc23b721142725813df2d75716
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-03-09 15:55:29 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -1,4 +1,4 @@
-#    Copyright (c) 2021 Infinity BOTs <https://t.me/Infinity_BOTs>
+#    Copyright (c) 2021 Infinity BOTs <https://t.me/MrC_VENOM>
  
 #    This program is free software: you can redistribute it and/or modify  
 #    it under the terms of the GNU General Public License as published by  
